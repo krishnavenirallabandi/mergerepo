614297c4975f9546a2c04f6994f5b5207596c792
krishnavenirallabandi <rallabandi.krishnaveni28@gmail.com>
2020-01-07 23:00:20 -0800

    added addition function in MergingClass

--- Mergingcode/src/com/merge/MergingClass.java
+++ Mergingcode/src/com/merge/MergingClass.java
@@ -1,9 +1,18 @@
 package com.merge;
 
 public class MergingClass {
+	int a=10,b=20,sum=0;
+	
+	//addition
+	public void add() {
+		sum=a+b;
+		System.out.println("addition is  "+sum);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MergingClass obj=new MergingClass();
+		obj.add();
+		
 
 	}
 
